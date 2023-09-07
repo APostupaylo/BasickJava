a4919dbf438c978da400f5edef2476b588be5138
APostupaylo <cool.filimohina@yandex.ru>
2023-09-07 11:20:41 +0200

    first commit

---
+++ src/main/java/anna/java/basic/Main.java
@@ -0,0 +1,19 @@
+package anna.java.basic;
+
+public class Main {
+        public static void main(String[] args) {
+            System.out.println("ѕеременные и типы данных");
+            int age; //создали переменную
+            age = 56; //задали переменной значение
+            //можно int age = 56;
+            System.out.println("¬озраст: "+ age + ".");
+            age = 45;
+            System.out.println(age);
+            float num = 4.34f;
+            double num1 = 5.3457;
+
+            char ch = 'c';
+            String user_name = "Alex";
+            boolean isHappy = false;
+}
+}
